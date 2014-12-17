e8b640d8fcbe11226ab6d324b7c56989eda06a79
lstarrsmith <logan29smith@gmail.com>
2014-12-17 15:56:25 -0500

    changed the connection file to hide ish

--- connection.rb
+++ connection.rb
@@ -1,10 +1,12 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection({
-  :adapter => "postgresql",
-  :host => "localhost",
-  :username => "Logan",
-  :database => "anagrams"
-  })
+ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/anagrams')
+
+# ActiveRecord::Base.establish_connection({
+#   :adapter => "postgresql",
+#   :host => "localhost",
+#   :username => "Logan",
+#   :database => "anagrams"
+#   })
 
   ActiveRecord::Base.logger = Logger.new(STDOUT)
